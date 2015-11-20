a1879779a9e53e21d64663c908c656f52fd361e7
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-11-20 23:30:24 +0100

    remove artendb design doc

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const rcs = require('./views/rcs.js')
 const faunaById = require('./views/faunaById.js')
 const fieldsOfGroup = require('./views/fieldsOfGroup.js')
 const floraById = require('./views/floraById.js')
+const artendb = require('./views/artendb.js')
 const macromycetesById = require('./views/macromycetesById.js')
 const mooseById = require('./views/mooseById.js')
 const objectsIdsByPcsName = require('./views/objectsIdsByPcsName.js')
@@ -16,6 +17,7 @@ const couchUrl = `http://${couchPass.user}:${couchPass.pass}@127.0.0.1:5984/ae`
 // const couchUrl = `http://${couchPass.user}:${couchPass.pass}@46.101.210.208:5984/ae`
 
 const db = new PouchDB(couchUrl, () => {
+  artendb(db)
   pcs(db)
   rcs(db)
   faunaById(db)
