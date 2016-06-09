f0ad1b1c45e24977ca1fd11cb30d949805da441b
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-09 14:10:14 +0200

    modularize and optimize

--- views/faunaById.js
+++ views/faunaById.js
@@ -1,59 +1,24 @@
-/*
- * creates a design doc and puts it into the db
- */
-
-'use strict'
-
-const ddoc = {
-  _id: '_design/faunaById',
-  views: {
-    faunaById: {
-      map: function (doc) {
-        function findStandardTaxonomyInDoc(dc) {
-          var standardtaxonomie = null
-          dc.Taxonomien.forEach(function (taxonomy) {
-            if (taxonomy.Standardtaxonomie) {
-              standardtaxonomie = taxonomy
-            }
-          })
-          return standardtaxonomie
-        }
-        if (
-          doc.Typ &&
-          doc.Typ === 'Objekt' &&
-          doc.Gruppe &&
-          doc.Gruppe === 'Fauna' &&
-          doc.Taxonomien
-        ) {
-          var standardtaxonomie = findStandardTaxonomyInDoc(doc)
-          if (standardtaxonomie) {
-            emit(standardtaxonomie.Eigenschaften['Taxonomie ID'], null)
-          }
-        }
-      }.toString()
-    }
-  }
-}
-
-module.exports = (db) => {
-  db.get('_design/faunaById')
-    .then((doc) => db.remove(doc))
-    .then(() => db.put(ddoc))
-    .then(() => {
-      console.log('faunaById index put')
-      return db.query('faunaById')
-    })
-    .then(() => console.log('faunaById index queried'))
-    .catch((error) => {
-      if (error.status === 404) {
-        // doc not found when getting
-        db.put(ddoc)
-          .then(() => {
-            console.log('faunaById index put')
-            return db.query('faunaById')
-          })
-          .then(() => console.log('faunaById index queried'))
-          .catch((err) => console.log(err))
+module.exports = function (doc) {
+  'use strict'
+  function findStandardTaxonomyInDoc(dc) {
+    var standardtaxonomie = null
+    dc.Taxonomien.forEach(function (taxonomy) {
+      if (taxonomy.Standardtaxonomie) {
+        standardtaxonomie = taxonomy
       }
     })
+    return standardtaxonomie
+  }
+  if (
+    doc.Typ &&
+    doc.Typ === 'Objekt' &&
+    doc.Gruppe &&
+    doc.Gruppe === 'Fauna' &&
+    doc.Taxonomien
+  ) {
+    var standardtaxonomie = findStandardTaxonomyInDoc(doc)
+    if (standardtaxonomie) {
+      emit(standardtaxonomie.Eigenschaften['Taxonomie ID'], null)
+    }
+  }
 }
--- views/fieldsOfGroup.js
+++ views/fieldsOfGroup.js
@@ -1,86 +1,50 @@
-/*
- * creates a design doc and puts it into the db
- */
-
-'use strict'
-
-const ddoc = {
-  _id: '_design/fieldsOfGroup',
-  views: {
-    fieldsOfGroup: {
-      map: function (doc) {
-        if (
-          doc.Gruppe &&
-          doc.Typ &&
-          doc.Typ === 'Objekt'
-        ) {
-          if (doc.Taxonomien) {
-            doc.Taxonomien.forEach(function (taxonomy) {
-              if (taxonomy.Eigenschaften) {
-                var eigenschaften = taxonomy.Eigenschaften
-                Object.keys(eigenschaften).forEach(function (feldname) {
-                  var feldwert = eigenschaften[feldname]
-                  emit([doc.Gruppe, 'taxonomy', taxonomy.Name, feldname, typeof feldwert], doc._id)
-                })
-              }
-            })
-          }
-
-          if (doc.Eigenschaftensammlungen) {
-            doc.Eigenschaftensammlungen.forEach(function (pc) {
-              if (pc.Eigenschaften) {
-                var eigenschaften = pc.Eigenschaften
-                Object.keys(eigenschaften).forEach(function (feldname) {
-                  var feldwert = eigenschaften[feldname]
-                  emit([doc.Gruppe, 'propertyCollection', pc.Name, feldname, typeof feldwert], doc._id)
-                })
-              }
-            })
-          }
-
-          if (doc.Beziehungssammlungen) {
-            doc.Beziehungssammlungen.forEach(function (beziehungssammlung) {
-              if (beziehungssammlung.Beziehungen && beziehungssammlung.Beziehungen.length > 0) {
-                beziehungssammlung.Beziehungen.forEach(function (beziehung) {
-                  Object.keys(beziehung).forEach(function (feldname) {
-                    var feldwert = beziehung[feldname]
-                    // irgendwie liefert dieser Loop auch Zahlen,
-                    // die aussehen als wären sie die keys eines Arrays. Ausschliessen
-                    if (isNaN(parseInt(feldname, 10))) {
-                      // jetzt loopen wir durch die Daten der Beziehung
-                      emit([doc.Gruppe, 'relation', beziehungssammlung.Name, feldname, typeof feldwert], doc._id)
-                    }
-                  })
-                })
-              }
-            })
-          }
+module.exports = function (doc) {
+  'use strict'
+  if (
+    doc.Gruppe &&
+    doc.Typ &&
+    doc.Typ === 'Objekt'
+  ) {
+    if (doc.Taxonomien) {
+      doc.Taxonomien.forEach(function (taxonomy) {
+        if (taxonomy.Eigenschaften) {
+          var eigenschaften = taxonomy.Eigenschaften
+          Object.keys(eigenschaften).forEach(function (feldname) {
+            var feldwert = eigenschaften[feldname]
+            emit([doc.Gruppe, 'taxonomy', taxonomy.Name, feldname, typeof feldwert], doc._id)
+          })
         }
-      }.toString(),
-      reduce: '_count'
+      })
+    }
+
+    if (doc.Eigenschaftensammlungen) {
+      doc.Eigenschaftensammlungen.forEach(function (pc) {
+        if (pc.Eigenschaften) {
+          var eigenschaften = pc.Eigenschaften
+          Object.keys(eigenschaften).forEach(function (feldname) {
+            var feldwert = eigenschaften[feldname]
+            emit([doc.Gruppe, 'propertyCollection', pc.Name, feldname, typeof feldwert], doc._id)
+          })
+        }
+      })
+    }
+
+    if (doc.Beziehungssammlungen) {
+      doc.Beziehungssammlungen.forEach(function (beziehungssammlung) {
+        if (beziehungssammlung.Beziehungen && beziehungssammlung.Beziehungen.length > 0) {
+          beziehungssammlung.Beziehungen.forEach(function (beziehung) {
+            Object.keys(beziehung).forEach(function (feldname) {
+              var feldwert = beziehung[feldname]
+              // irgendwie liefert dieser Loop auch Zahlen,
+              // die aussehen als wären sie die keys eines Arrays. Ausschliessen
+              if (isNaN(parseInt(feldname, 10))) {
+                // jetzt loopen wir durch die Daten der Beziehung
+                emit([doc.Gruppe, 'relation', beziehungssammlung.Name, feldname, typeof feldwert], doc._id)
+              }
+            })
+          })
+        }
+      })
     }
   }
-}
-
-module.exports = (db) => {
-  db.get('_design/fieldsOfGroup')
-    .then((doc) => db.remove(doc))
-    .then(() => db.put(ddoc))
-    .then(() => {
-      console.log('fieldsOfGroup index put')
-      return db.query('fieldsOfGroup')
-    })
-    .then(() => console.log('fieldsOfGroup index queried'))
-    .catch((error) => {
-      if (error.status === 404) {
-        // doc not found when getting
-        db.put(ddoc)
-          .then(() => {
-            console.log('fieldsOfGroup index put')
-            return db.query('fieldsOfGroup')
-          })
-          .then(() => console.log('fieldsOfGroup index queried'))
-          .catch((err) => console.log(err))
-      }
-    })
 }
