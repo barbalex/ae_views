d9a242b69649d0714d3727e66f16b1418098f732
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-09 14:09:52 +0200

    modularize and optimize

--- views/tOByTaxonomy.js
+++ views/tOByTaxonomy.js
@@ -1,44 +1,9 @@
-/*
- * creates a design doc and puts it into the db
- */
-
-'use strict'
-
-const ddoc = {
-  _id: '_design/tOByTaxonomy',
-  views: {
-    tOByTaxonomy: {
-      map: function (doc) {
-        if (
-          doc.Typ &&
-          doc.Typ === 'Taxonomie-Objekt'
-        ) {
-          emit([doc.Taxonomie, doc._id], null)
-        }
-      }.toString()
-    }
+module.exports = function (doc) {
+  'use strict'
+  if (
+    doc.Typ &&
+    doc.Typ === 'Taxonomie-Objekt'
+  ) {
+    emit([doc.Taxonomie, doc._id], null)
   }
-}
-
-module.exports = (db) => {
-  db.get('_design/tOByTaxonomy')
-    .then((doc) => db.remove(doc))
-    .then(() => db.put(ddoc))
-    .then(() => {
-      console.log('tOByTaxonomy index put')
-      return db.query('tOByTaxonomy')
-    })
-    .then(() => console.log('tOByTaxonomy index queried'))
-    .catch((error) => {
-      if (error.status === 404) {
-        // doc not found when getting
-        db.put(ddoc)
-          .then(() => {
-            console.log('tOByTaxonomy index put')
-            return db.query('tOByTaxonomy')
-          })
-          .then(() => console.log('tOByTaxonomy index queried'))
-          .catch((err) => console.log(err))
-      }
-    })
 }
--- views/tcs.js
+++ views/tcs.js
@@ -1,60 +1,24 @@
-/*
- * creates a design doc and puts it into the db
- */
-
-'use strict'
-
-const ddoc = {
-  _id: '_design/tcs',
-  views: {
-    tcs: {
-      map: function (doc) {
+module.exports = function (doc) {
+  'use strict'
+  if (
+    doc.Typ &&
+    doc.Typ === 'Objekt' &&
+    doc.Gruppe &&
+    doc.Taxonomien
+  ) {
+    doc.Taxonomien.forEach(function (tc) {
+      // add pcZusammenfassend
+      var standard = !!tc.Standardtaxonomie
+      var felder = {}
+      Object.keys(tc).forEach(function (key) {
         if (
-          doc.Typ &&
-          doc.Typ === 'Objekt' &&
-          doc.Gruppe &&
-          doc.Taxonomien
+          key !== 'Name' &&
+          key !== 'Eigenschaften'
         ) {
-          doc.Taxonomien.forEach(function (tc) {
-            // add pcZusammenfassend
-            var standard = !!tc.Standardtaxonomie
-            var felder = {}
-            Object.keys(tc).forEach(function (key) {
-              if (
-                key !== 'Name' &&
-                key !== 'Eigenschaften'
-              ) {
-                felder[key] = tc[key]
-              }
-            })
-            emit([doc.Gruppe, standard, tc.Name, tc['Organisation mit Schreibrecht'], felder], null)
-          })
+          felder[key] = tc[key]
         }
-      }.toString(),
-      reduce: '_count'
-    }
+      })
+      emit([doc.Gruppe, standard, tc.Name, tc['Organisation mit Schreibrecht'], felder], null)
+    })
   }
-}
-
-module.exports = (db) => {
-  db.get('_design/tcs')
-    .then((doc) => db.remove(doc))
-    .then(() => db.put(ddoc))
-    .then(() => {
-      console.log('tcs index put')
-      return db.query('tcs')
-    })
-    .then(() => console.log('tcs index queried'))
-    .catch((error) => {
-      if (error.status === 404) {
-        // doc not found when getting
-        db.put(ddoc)
-          .then(() => {
-            console.log('tcs index put')
-            return db.query('tcs')
-          })
-          .then(() => console.log('tcs index queried'))
-          .catch((err) => console.log(err))
-      }
-    })
 }
